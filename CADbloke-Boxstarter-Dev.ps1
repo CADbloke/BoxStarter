b7749a6dec3aab95639b3b1188305f767d2493fd
Ewen @ 8560w <Ewen@CADbloke.com>
2015-02-01 20:38:55 +1100

    Added BoxStarter Scripts

---
+++ CADbloke-Boxstarter-Dev.ps1
@@ -0,0 +1,73 @@
+######################################################
+# Before you run this, run http://boxstarter.org/package/url?https://j.mp/CADbloke-boxstarter
+######################################################
+
+# This is http://boxstarter.org/package/url?https://j.mp/CADbloke-boxstarter-dev
+
+cinst lessmsi  # MSI explorer
+cinst fiddler4
+cinst wireshark
+cinst markdownpad2
+cinst linqpad
+cinst SublimeText3 
+cinst atom # https://atom.io/
+
+######################################################
+# Set Folders and Environment Variables
+######################################################
+Write-Host "Setting Folders and Environment Variables"
+if(!(Test-Path "c:\Codez")) { New-Item -Type Directory "C:\Codez"}
+if(!(Test-Path "d:\CodezOnD")) { New-Item -Type Directory "d:\CodezOnD"}  # quite useless on an Azure VM
+
+Install-ChocolateyEnvironmentVariable "CODEZ" "c:\codez"
+Install-ChocolateyEnvironmentVariable "D-CODEZ" "d:\CodezOnD"
+Write-Host
+
+######################################################
+# Visual Studio
+######################################################
+Write-Host "Installing Visual Studio 2013 Ultimate"
+cinst VisualStudio2013Ultimate
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+cinst nunit
+cinst xunit
+cinst resharper -Version 8.2.3000.5176  # remove version info it you wan the latest
+cinst dotpeek
+cinst vs2013sdk
+cinst wixtoolset
+
+Install-ChocolateyVsixPackage NuGetPackageManager https://visualstudiogallery.msdn.microsoft.com/4ec1526c-4a8c-4a84-b702-b21a8f5293ca/file/105933/7/NuGet.Tools.2013.vsix
+Install-ChocolateyVsixPackage CodeAlignment https://visualstudiogallery.msdn.microsoft.com/7179e851-a263-44b7-a177-1d31e33c84fd/file/32256/37/CodeAlignment.vsix
+Install-ChocolateyVsixPackage GitSourceControlProvider https://visualstudiogallery.msdn.microsoft.com/63a7e40d-4d71-4fbb-a23b-d262124b8f4c/file/29105/46/GitSccProvider.vsix
+Install-ChocolateyVsixPackage IntelliCommand https://visualstudiogallery.msdn.microsoft.com/83f59659-abc1-4bfa-9779-42f687af0481/file/87872/3/IntelliCommand.vsix
+Install-ChocolateyVsixPackage MultiEditing https://visualstudiogallery.msdn.microsoft.com/2beb9705-b568-45d1-8550-751e181e3aef/file/93630/8/MultiEdit.vsix
+Install-ChocolateyVsixPackage OpeninExternalBrowser https://visualstudiogallery.msdn.microsoft.com/46c0c49e-f825-454b-9f6a-48b216797eb5/file/136677/1/Tvl.VisualStudio.OpenInExternalBrowser.vsix
+cinst VS2013.PowerTools
+Install-ChocolateyVsixPackage SideWaffle https://visualstudiogallery.msdn.microsoft.com/a16c2d07-b2e1-4a25-87d9-194f04e7a698/referral/110630
+Install-ChocolateyVsixPackage SQLCompactToolbox https://visualstudiogallery.msdn.microsoft.com/0e313dfd-be80-4afb-b5e9-6e74d369f7a1/file/29445/72/SqlCeToolbox.vsix
+Install-ChocolateyVsixPackage VisualHg2 https://bitbucket.org/lmn/visualhg2/downloads/VisualHg.vsix
+Install-ChocolateyVsixPackage cinst VS2013.VSCommands
+Install-ChocolateyVsixPackage XAMLstyler https://visualstudiogallery.msdn.microsoft.com/3de2a3c6-def5-42c4-924d-cc13a29ff5b7/file/124126/11/XamlStyler.Package.vsix
+
+######################################################
+# Pin the Taskbar icons
+######################################################
+Write-Host "Pinning Apps to the TaskBar"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles(x86)\Microsoft Visual Studio 12.0\Blend\Blend.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles(x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+Write-Host
+
+
+
+######################################################
+# Windows Updates
+######################################################
+Install-WindowsUpdate -AcceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
+
+enable-UAC # disbled at the start of this script
+
+Write-ChocolateySuccess 'Dev Cloud VM'
+Exit
---
+++ CADbloke-Boxstarter.ps1
@@ -0,0 +1,131 @@
+######################################################
+# To run this, use Internet Exploder and go to http://boxstarter.org/package/nr/url?https://j.mp/CADbloke-boxstarter
+######################################################
+# moar instructions at http://boxstarter.org/Learn/WebLauncher
+
+# Boxstarter Options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+# Boxstarter (not Chocolatey) commands
+Update-ExecutionPolicy Unrestricted
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+# Enable-RemoteDesktop  # already enabled on Azure VMs and no thanks for my laptop.
+Disable-InternetExplorerESC  #Turns off IE Enhanced Security Configuration that is on by default on Server OS versions
+Disable-UAC  # at least until this is over
+
+Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableShowHiddenFilesFoldersDrives 
+Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableShowAppsViewOnStartScreen -EnableSearchEverywhereInAppsView -EnableListDesktopAppsFirst
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+disable-computerrestore -drive "C:\"  # http://ss64.com/ps/disable-computerrestore.html
+if (Test-PendingReboot) { Invoke-Reboot }
+
+######################################################
+# General Apps
+######################################################
+Write-Host "Installing applications from Chocolatey"
+cinst DotNet3.5 # not installed by default on Windows Server 2012
+if (Test-PendingReboot) { Invoke-Reboot }
+
+cinst sourcecodepro
+cinst notepadplusplus.install
+cinst GoogleChrome
+cinst 7zip.install
+cinst lastpass
+cinst sysinternals
+cinst paint.net
+cinst dropbox
+cinst irfanview
+cinst foxitreader
+cinst vlc
+cinst windirstat
+cinst clipx  # Clipboard history manager
+cinst beyondcompare -Version 3.3.12.18414
+cinst ccleaner
+cinst malwarebytes
+cinst flashplayeractivex
+cinst virtualclonedrive
+cinst console-devel   # a better CMD
+cinst grepwin
+
+######################################################
+# Git & Hg
+######################################################
+cinst tortoisehg
+cinst git.install -Version 1.9.2  # Later versions are broken with SVN
+cinst tortoisegit
+cinst sourcetree
+cinst githubforwindows
+Restart-Explorer   # Boxstarter-specific command
+if (Test-PendingReboot) { Invoke-Reboot }
+
+
+######################################################
+# Pin the Taskbar icons
+######################################################
+Write-Host "Pinning Apps to the TaskBar"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles(x86)\Google\Chrome\Application\chrome.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles(x86)\IrfanView\i_view32.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\Notepad++\notepad++.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\TortoiseHg\thgw.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:windir\explorer.exe"
+Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
+Write-Host
+
+######################################################
+# Set Folders and Environment Variables
+######################################################
+Write-Host "Setting DropBox Environment Variable"
+Install-ChocolateyEnvironmentVariable "DROPBOX" "%HOMEPATH%\Dropbox"  # let's see if that works
+Write-Host
+
+
+######################################################
+# Add Git to the path
+######################################################
+Write-Host "Adding Git\bin to the path"
+Add-Path "C:\Program Files (x86)\Git\bin"
+Write-Host
+
+######################################################
+# Set file type associations
+######################################################
+Write-Host "Setting file type associations"
+Install-ChocolateyFileAssociation ".txt" "$env:programfiles\Notepad++\notepad++.exe"
+Install-ChocolateyFileAssociation ".log" "$env:programfiles\Notepad++\notepad++.exe"
+Install-ChocolateyFileAssociation ".md" "$env:programfiles\MarkdownPad 2\MarkdownPad2.exe"
+Write-Host
+Write-Host "Finished."
+
+Exit
+#
+# End of Chocolatey
+#
+###########################################################################################################
+
+# https://gist.github.com/vintem/6334646
+function Add-Path() {
+    [Cmdletbinding()]
+    param([parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)][String[]]$AddedFolder)
+    # Get the current search path from the environment keys in the registry.
+    $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+    # See if a new folder has been supplied.
+    if (!$AddedFolder) {
+        Return 'No Folder Supplied. $ENV:PATH Unchanged'
+    }
+    # See if the new folder exists on the file system.
+    if (!(TEST-PATH $AddedFolder))
+    { Return 'Folder Does not Exist, Cannot be added to $ENV:PATH' }cd
+    # See if the new Folder is already in the path.
+    if ($ENV:PATH | Select-String -SimpleMatch $AddedFolder)
+    { Return 'Folder already within $ENV:PATH' }
+    # Set the New Path
+    $NewPath=$OldPath+';'+$AddedFolder
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
+    # Show our results back to the world
+    Return $NewPath
+}
