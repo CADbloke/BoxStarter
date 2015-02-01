3215c224f23f5aea7da967856e203c77fa0514a0
Ewen @ 8560w <Ewen@CADbloke.com>
2015-02-01 23:09:19 +1100

    oh, so you have to declare a function **before** you use it. Oh.

--- CADbloke-Boxstarter.ps1
+++ CADbloke-Boxstarter.ps1
@@ -1,3 +1,28 @@
+# https://gist.github.com/vintem/6334646
+function Add-Path() {
+    [Cmdletbinding()]
+    param([parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)][String[]]$AddedFolder)
+    # Get the current search path from the environment keys in the registry.
+    $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+    # See if a new folder has been supplied.
+    if (!$AddedFolder) {
+        Return 'No Folder Supplied. $ENV:PATH Unchanged'
+    }
+    # See if the new folder exists on the file system.
+    if (!(TEST-PATH $AddedFolder))
+    { Return 'Folder Does not Exist, Cannot be added to $ENV:PATH' }cd
+    # See if the new Folder is already in the path.
+    if ($ENV:PATH | Select-String -SimpleMatch $AddedFolder)
+    { Return 'Folder already within $ENV:PATH' }
+    # Set the New Path
+    $NewPath=$OldPath+';'+$AddedFolder
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
+    # Show our results back to the world
+    Return $NewPath
+}
+
+# Begin BoxStarter + Chocolatey
+
 ######################################################
 # To run this, use Internet Exploder and go to http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/CADbloke/BoxStarter/master/CADbloke-Boxstarter.ps1
 ######################################################
@@ -105,26 +130,3 @@ Exit
 # End of Chocolatey
 #
 ###########################################################################################################
-
-# https://gist.github.com/vintem/6334646
-function Add-Path() {
-    [Cmdletbinding()]
-    param([parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)][String[]]$AddedFolder)
-    # Get the current search path from the environment keys in the registry.
-    $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-    # See if a new folder has been supplied.
-    if (!$AddedFolder) {
-        Return 'No Folder Supplied. $ENV:PATH Unchanged'
-    }
-    # See if the new folder exists on the file system.
-    if (!(TEST-PATH $AddedFolder))
-    { Return 'Folder Does not Exist, Cannot be added to $ENV:PATH' }cd
-    # See if the new Folder is already in the path.
-    if ($ENV:PATH | Select-String -SimpleMatch $AddedFolder)
-    { Return 'Folder already within $ENV:PATH' }
-    # Set the New Path
-    $NewPath=$OldPath+';'+$AddedFolder
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
-    # Show our results back to the world
-    Return $NewPath
-}
